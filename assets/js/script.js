593dff66022eec471e99ffdb08103e0f73a01a87
xStephx <bojkovskistefan9@gmail.com>
2024-11-30 13:22:00 +0100

    fixed style on pricing components

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,6 @@ toggle.addEventListener("change", () => {
 
     prices.forEach((priceElement) => {
         const newPrice = isMonthly ? priceElement.dataset.annually : priceElement.dataset.monthly
-        priceElement.innerHTML = `&dollar;${newPrice}`
+        priceElement.innerHTML = `<span class="text-5xl mr-1">&dollar;</span>${newPrice}`
     })
 })
